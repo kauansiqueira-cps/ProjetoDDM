07e2d3ef0638a36b83a03293a2abaae9d93b98c4
pedrosantos-cps <pedro.santos898@etec.sp.gov.br>
2024-03-12 12:01:56 -0300

    botao

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button} from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Pedro Henrique de Oliveira Dos Santos</Text>
       <Text>Kauan Guilherme Siqueira</Text>
       <Text>05/03/2024 DDM 3B3 Lado B</Text>
-
       <Text>Novo Commit Clone</Text>
+
+      <Button title='Salvar'></Button>
+      
       <StatusBar style="auto" />
     </View>
   );
